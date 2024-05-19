424ef5bf41a0b061315237ee76f6c43ac84d30df
iamvides <164425880+iamvides@users.noreply.github.com>
2024-05-19 16:56:28 -0500

    Update Actividad2.cpp

--- SESION 7/EXPERIENCIAS/Actividad2.cpp
+++ SESION 7/EXPERIENCIAS/Actividad2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Cliente {
@@ -9,61 +8,67 @@ protected:
     string direccion;
     string telefono;
     string email;
-    string numeroTarjeta;
-    string codigoSeguridad;
-
-protected:
-    string encriptar(string &data) {
-        string encrypted = data;
-        char key = 'K'; 
-        for (size_t i = 0; i < data.size(); ++i) {
-            encrypted[i] ^= key;
+    string password;
+    string respuestaC;
+    string encriptar(string data) {
+        string encrypted;
+        for (size_t i = 0; i < data.size(); i++) {
+            char c = data[i];
+            if (c >= 'a' && c <= 'z') {
+                encrypted += 'z' - (c - 'a');
+            } else if (c >= 'A' && c <= 'Z') {
+                encrypted += 'Z' - (c - 'A');
+            } else {
+                encrypted += c; // No encriptar otros caracteres
+            }
         }
         return encrypted;
     }
 
 public:
-    Cliente(string &n, string &d, string &t, string &e, string &nt, string &cs)
-        : nombre(n), direccion(d), telefono(t), email(e), numeroTarjeta(nt), codigoSeguridad(cs) {}
+    Cliente(string n, string d, string t, string e, string pass, string respu)
+        : nombre(n), direccion(d), telefono(t), email(e), password(pass), respuestaC(respu) {}
 
     string getNombre() { return nombre; }
     string getDireccion() { return direccion; }
     string getTelefono() { return telefono; }
     string getEmail() { return email; }
-    string getNumeroTarjeta() { return numeroTarjeta; }
-    string getCodigoSeguridad() { return codigoSeguridad; }
-
-    void encriptarInformacion() {
-        numeroTarjeta = encriptar(numeroTarjeta);
-        codigoSeguridad = encriptar(codigoSeguridad);
-    }
+    string getPassword() { return password; }
+    string getRespuestaC() { return respuestaC; }
 };
 
 class ClienteSeguro : public Cliente {
 public:
-    ClienteSeguro(string &n, string &d, string &t, string &e, string &nt, string &cs : Cliente(n, d, t, e, nt, cs) {}
+    ClienteSeguro(string _nombre, string _direccion, string _telefono, string _email, string _password, string _respuestac)
+        : Cliente(_nombre, _direccion, _telefono, _email, _password, _respuestac) {}
 
-    bool verificarAutenticidad(string &nombre, string &codigo) {
-        return (this->nombre == nombre && this->codigoSeguridad == codigo);
+    string getPasswordencriptado() {
+        return encriptar(password);
+    }
+
+    string getRespuestaCencriptada() {
+        return encriptar(respuestaC);
+    }
+
+    bool verificarAutenticidad(string nombre, string con) {
+        return (nombre == this->nombre && getPassword() == con);
     }
 };
 
 int main() {
-    ClienteSeguro cliente("Alexander Velasquez", "Av. Chile 456", "123456789", "alexandra@ucsm.edu.pe", "1234567890123456", "123");
-    cliente.encriptarInformacion();
-
+    ClienteSeguro cliente("Alexander Velasquez", "Av. Chile 456", "123456789", "alexandra@ucsm.edu.pe", "holamundo", "colorFavorito?");
     cout << "Nombre: " << cliente.getNombre() << endl;
     cout << "Direccion: " << cliente.getDireccion() << endl;
     cout << "Telefono: " << cliente.getTelefono() << endl;
     cout << "Email: " << cliente.getEmail() << endl;
-    cout << "Tarjeta Encriptada: " << cliente.getNumeroTarjeta() << endl;
-    cout << "Codigo Seguridad Encriptado: " << cliente.getCodigoSeguridad() << endl;
-
-    if (cliente.verificarAutenticidad("Alexander Velasquez", "123")) {
+    cout << "Contraseña Encriptada: " << cliente.getPasswordencriptado() << endl;
+    cout << "Codigo Seguridad Encriptado: " << cliente.getRespuestaCencriptada() << endl;
+    if (cliente.verificarAutenticidad("Alexander Velasquez", "holamundo")) {
         cout << "Autenticidad verificada." << endl;
     } else {
         cout << "Autenticidad no verificada." << endl;
     }
-
     return 0;
 }
+
+
