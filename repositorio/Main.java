cf4b4c9b0de9011e4382af9e91de8b29dbc184b7
iamvides <164425880+iamvides@users.noreply.github.com>
2024-09-10 10:54:47 -0500

    Update Main.java

--- repositorio/Main.java
+++ repositorio/Main.java
@@ -1,33 +1,33 @@
 interface Instrumento { //Definimos la interfas
-    void tocar(); //
-    void afinar();
+    void tocar(); // defnicomos funcion
+    void afinar(); // definimos funcion
 }
 
-class Piano implements Instrumento {
+class Piano implements Instrumento { // clase piano que implementa la interfas
     @Override
-    public void tocar() {
+    public void tocar() { // sobreescritura
         System.out.println("Piano tocado con exito");
     }
 
     @Override
-    public void afinar() {
+    public void afinar() { // sobreescritura
         System.out.println("Piano afinado con exito");
     }
 }
 
-class Saxo implements Instrumento {
+class Saxo implements Instrumento { // calse saxo que implementa la iterfas
     @Override
-    public void tocar() {
+    public void tocar() { // sobreescritura
         System.out.println("Saxo tocado con exito");
     }
 
     @Override
-    public void afinar() {
+    public void afinar() { // sobreescritura
         System.out.println("Saxo afinado con exito");
     }
 }
 
-public class Main {
+public class Main { // calse main
     public static void main(String[] args) {
         Instrumento[] instrumentos = {new Piano(), new Saxo()};
 
