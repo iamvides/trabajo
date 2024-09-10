89af5e1fdf7d444ad6f662e98d8c59502b923071
iamvides <164425880+iamvides@users.noreply.github.com>
2024-09-10 10:52:56 -0500

    Update and rename Tienda.java to Main.java

---
+++ repositorio/Main.java
@@ -0,0 +1,39 @@
+interface Instrumento { //Definimos la interfas
+    void tocar(); //
+    void afinar();
+}
+
+class Piano implements Instrumento {
+    @Override
+    public void tocar() {
+        System.out.println("Piano tocado con exito");
+    }
+
+    @Override
+    public void afinar() {
+        System.out.println("Piano afinado con exito");
+    }
+}
+
+class Saxo implements Instrumento {
+    @Override
+    public void tocar() {
+        System.out.println("Saxo tocado con exito");
+    }
+
+    @Override
+    public void afinar() {
+        System.out.println("Saxo afinado con exito");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Instrumento[] instrumentos = {new Piano(), new Saxo()};
+
+        for (Instrumento instrumento : instrumentos) {
+            instrumento.tocar();
+            instrumento.afinar();
+        }   
+    }
+}
